8499a349885f759151f9d3ad5df73e52cd1d6c0b
SamsetDev <singh.891sanjay@gmail.com>
2016-04-27 11:55:41 +0530

    dfp ads
    dip ads addd

--- Ads/app/src/main/java/com/samset/ads/doubleclickpublisher_DFP/DFP_InterstialAds.java
+++ Ads/app/src/main/java/com/samset/ads/doubleclickpublisher_DFP/DFP_InterstialAds.java
@@ -1,8 +1,12 @@
 package com.samset.ads.doubleclickpublisher_DFP;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.AdListener;
@@ -11,12 +15,19 @@ import com.google.android.gms.ads.doubleclick.PublisherInterstitialAd;
 import com.samset.ads.R;
 
 public class DFP_InterstialAds extends AppCompatActivity {
+    private static final long GAME_LENGTH_MILLISECONDS = 3000;
+
     private PublisherInterstitialAd mInterstitialAd;
+    private CountDownTimer mCountDownTimer;
+    private Button mRetryButton;
+    private boolean mGameIsInProgress;
+    private boolean mAdIsLoading;
+    private long mTimerMilliseconds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dfp__interstial_ads);
-
+        // Create the InterstitialAd and set the adUnitId.
         mInterstitialAd = new PublisherInterstitialAd(this);
         // Defined in res/values/strings.xml
         mInterstitialAd.setAdUnitId(getString(R.string.dfp_interstialads_unit_id));
@@ -24,28 +35,102 @@ public class DFP_InterstialAds extends AppCompatActivity {
         mInterstitialAd.setAdListener(new AdListener() {
             @Override
             public void onAdClosed() {
-                displayInterstitial();
+                startGame();
             }
 
             @Override
             public void onAdLoaded() {
-                Log.e("DFP"," AdLoaded");
+                mAdIsLoading = false;
             }
 
             @Override
             public void onAdFailedToLoad(int errorCode) {
-                Log.e("DFP"," AdFailedToLoad");
+                mAdIsLoading = false;
             }
         });
 
-        PublisherAdRequest adRequest = new PublisherAdRequest.Builder().build();
-        mInterstitialAd.loadAd(adRequest);
+        // Create the "retry" button, which tries to show an interstitial between game plays.
+        mRetryButton = ((Button) findViewById(R.id.retry_button));
+        mRetryButton.setVisibility(View.INVISIBLE);
+        mRetryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showInterstitial();
+            }
+        });
 
+        startGame();
     }
-    public void displayInterstitial() {
-        // If Ads are loaded, show Interstitial else show nothing.
-        if (mInterstitialAd.isLoaded()) {
-            mInterstitialAd.show();
+
+    private void createTimer(final long milliseconds) {
+        // Create the game timer, which counts down to the end of the level
+        // and shows the "retry" button.
+        if (mCountDownTimer != null) {
+            mCountDownTimer.cancel();
         }
+
+        final TextView textView = ((TextView) findViewById(R.id.timer));
+
+        mCountDownTimer = new CountDownTimer(milliseconds, 50) {
+            @Override
+            public void onTick(long millisUnitFinished) {
+                mTimerMilliseconds = millisUnitFinished;
+                textView.setText("seconds remaining: " + ((millisUnitFinished / 1000) + 1));
+            }
+
+            @Override
+            public void onFinish() {
+                mGameIsInProgress = false;
+                textView.setText("done!");
+                mRetryButton.setVisibility(View.VISIBLE);
+            }
+        };
+    }
+
+    @Override
+    public void onResume() {
+        // Start or resume the game.
+        super.onResume();
+
+        if (mGameIsInProgress) {
+            resumeGame(mTimerMilliseconds);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        // Cancel the timer if the game is paused.
+        mCountDownTimer.cancel();
+        super.onPause();
+    }
+
+    private void showInterstitial() {
+        // Show the ad if it's ready. Otherwise toast and restart the game.
+        if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
+            startGame();
+        }
+    }
+
+    private void startGame() {
+        // Request a new ad if one isn't already loaded, hide the button, and kick off the timer.
+        if (!mAdIsLoading && !mInterstitialAd.isLoaded()) {
+            mAdIsLoading = true;
+            PublisherAdRequest adRequest = new PublisherAdRequest.Builder().build();
+            mInterstitialAd.loadAd(adRequest);
+        }
+
+        mRetryButton.setVisibility(View.INVISIBLE);
+        resumeGame(GAME_LENGTH_MILLISECONDS);
+    }
+
+    private void resumeGame(long milliseconds) {
+        // Create a new timer for the correct length and start it.
+        mGameIsInProgress = true;
+        mTimerMilliseconds = milliseconds;
+        createTimer(milliseconds);
+        mCountDownTimer.start();
     }
 }
